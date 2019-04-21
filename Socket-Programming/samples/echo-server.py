d7c790cf645d1be1802102847a84e4b422ce2714
Parham Alvani <parham.alvani@gmail.com>
2019-04-21 19:33:08 +0000

    Adds echo-server.py.

---
+++ Socket-Programming/samples/echo-server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# In The Name of God
+# =======================================
+# [] File Name : echo-server.py
+#
+# [] Creation Date : 21-04-2019
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 1373         # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
